b74c86dc9250a71785634de4f48bc0d4d01b9c12
Michael Johnson <michael@johnson.computer>
2019-03-08 16:43:08 -0500

    pre-commit.sh: Fix clang-format checker

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-git diff --cached --name-only --diff-filter=ACMRT |
-  grep "\.java$" | tr '\n' '\0' |
-  xargs -0 -n1 clang-format -style=file -output-replacements-xml |
-  grep "<replacement " >/dev/null
-if [ $? -ne 1 ]; then 
-    echo "Committed code is not clang-formatted"
-    exit 1
-fi
+SAVEIFS=$IFS
+IFS=$'\n'
+CHANGED_FILES=($(git diff --cached --name-only --diff-filter=ACMRT | grep '\.java$'))
+IFS=$SAVEIFS
+unset SAVEIFS
+
+FAILED=0
+
+for CHANGED_FILE in "${CHANGED_FILES[@]}"; do
+	clang-format -style=file -output-replacements-xml "${CHANGED_FILE}" | grep '<replacement '>/dev/null
+	if [ $? -ne 1 ]; then
+		echo "${CHANGED_FILE}: Not clang-formatted"
+		FAILED=1
+	fi
+done
+
+exit $FAILED
