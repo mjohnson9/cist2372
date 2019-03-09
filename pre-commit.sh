830ded00b465a56f13e5f6d0700643666d3ef817
Michael Johnson <michael@johnson.computer>
2019-03-09 06:46:49 -0500

    pre-commit.sh: Use actual file contents being committed instead of filesystem contents

--- pre-commit.sh
+++ pre-commit.sh
@@ -9,7 +9,7 @@ unset SAVEIFS
 FAILED=0
 
 for CHANGED_FILE in "${CHANGED_FILES[@]}"; do
-	clang-format -style=file -output-replacements-xml "${CHANGED_FILE}" | grep '<replacement '>/dev/null
+	git show ":${CHANGED_FILE}" | clang-format -style=file -output-replacements-xml -assume-filename="${CHANGED_FILE}" - | grep '<replacement '>/dev/null
 	if [ $? -ne 1 ]; then
 		echo "${CHANGED_FILE}: Not clang-formatted"
 		FAILED=1
